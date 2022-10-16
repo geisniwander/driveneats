c95c9416f48df6839fda17a3410b095cdf79f516
geisniwander <geisniwander@gmail.com>
2022-10-15 22:11:48 -0300

    tratamento_de_selecoes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,58 @@
-function ativaBorda(idItem){
-    const id="#"+idItem;
-    const idb="#b"+idItem;
-    const elemento= document.querySelector(id);
-    const botao= document.querySelector(idb);
-    elemento.classList.toggle('borda-verde');
-    botao.classList.toggle('escondido');
+let pratosSelecionados=0;
+let bebidasSelecionadas=0;
+let sobremesasSelecionadas=0;
+
+
+function selecionaPrato(idItem){
+    const selecionado=document.querySelector(".prato.borda-verde");
+    if(selecionado!==null){
+        selecionado.classList.remove('borda-verde');
+        const sel=selecionado.querySelector('.icone-confirma');
+        sel.classList.add('escondido');
+    }
+    idItem.classList.toggle('borda-verde');
+    const bselecionado= idItem.querySelector('.icone-confirma');
+    bselecionado.classList.toggle('escondido');
+    pratosSelecionados=1;
+    ativaBotao();
+    }
+    
+function selecionaBebida(idItem){
+    const b_selecionado=document.querySelector(".bebida.borda-verde");
+    if(b_selecionado!==null){
+        b_selecionado.classList.remove('borda-verde');
+        const sel=b_selecionado.querySelector('.icone-confirma');
+        sel.classList.add('escondido');
+        bebidasSelecionadas=0;
+    }
+    idItem.classList.toggle('borda-verde');
+    const bselecionado= idItem.querySelector('.icone-confirma');
+    bselecionado.classList.toggle('escondido');
+    bebidasSelecionadas=1;
+    ativaBotao();
 }
+
+function selecionaSobremesa(idItem){
+    const s_selecionado= document.querySelector('.sobremesa.borda-verde');
+    if(s_selecionado!==null){
+        s_selecionado.classList.remove('borda-verde');
+        const sel=s_selecionado.querySelector('.icone-confirma');
+        sel.classList.add('escondido');
+        sobremesasSelecionadas=0;
+    }
+    idItem.classList.toggle('borda-verde');
+    const bselecionado= idItem.querySelector('.icone-confirma');
+    bselecionado.classList.toggle('escondido');
+    sobremesasSelecionadas=1;
+    ativaBotao();
+}
+
+function ativaBotao(){
+
+    if(pratosSelecionados==1 & bebidasSelecionadas==1 & sobremesasSelecionadas==1){
+    const botao=document.querySelector('.selecione-itens');
+    botao.classList.add('confirmacao');
+}
+}
+
+
